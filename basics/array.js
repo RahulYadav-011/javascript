08c2fba8b2249aa8085e3690b121c80c6e808f73
RahulYadav-011 <141146631+RahulYadav-011@users.noreply.github.com>
2023-08-03 08:26:41 +0000

    json

---
+++ basics/array.js
@@ -0,0 +1,4 @@
+const name1 = ["Rahul","Raman","Rohit"];
+const name2= ["Yadav","yaduvanshi","Pushpa"];
+const Name3= [...name1,...name2];
+console.log(Name3);
---
+++ basics/object.js
@@ -0,0 +1,7 @@
+const user={
+name:"Rahul",
+email:"yadavrahul@gmail.com",
+city:"agra",
+age:21
+}
+console.log(user["email"]) 
