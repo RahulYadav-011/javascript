e23c95a8f8a1094bbec9f9395b7c2496354dbcb7
RahulYadav-011 <141146631+RahulYadav-011@users.noreply.github.com>
2023-08-03 08:51:01 +0000

    function

---
+++ basics/function.js
@@ -0,0 +1,9 @@
+// function SayMyName(a,b) {
+//     return a+b;
+// }
+//  let fun= SayMyName(4,2)
+// console.log("two number Add :",fun)
+// const table= 2;
+// for(let i =1;i<11;i++){
+//     console.log(2*i);
+// }
